f1a3e5d956a30fc349df968227bc4541efaa3eb7
ShyamSwami2414 <shyamswami2002@gmail.com>
2026-05-01 16:21:47 +0530

    server changes

--- src/routes/userRoute/index.js
+++ src/routes/userRoute/index.js
@@ -1,6 +1,6 @@
-const apiPrefix = "/user";
+const apiPrefix = "/api/user";
 module.exports = (app) => {
-  app.use(`/`, require("./userAuthRoute"));
+  app.use(`/api`, require("./userAuthRoute"));
 
   app.use(`${apiPrefix}/role`, require("./roleRoute"));
   app.use(`${apiPrefix}/kyc`, require("./kycRoute"));
