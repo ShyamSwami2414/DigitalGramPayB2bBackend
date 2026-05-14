712558f8a13b7812680b8cd8c4b4f58c60f504a6
ShyamSwami2414 <shyamswami2002@gmail.com>
2026-05-14 16:24:15 +0530

    chnges

--- src/utils/generateUniqueReferenceId.js
+++ src/utils/generateUniqueReferenceId.js
@@ -1,5 +1,15 @@
 const { nanoid } = require("nanoid");
 
-exports.generateUniqueRefernceId = () => {
-  return `REF-${nanoid(10)}`;
+const nanoid = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 8);
+
+exports.generateUniqueRefernceId = (prefix = "TXN") => {
+  const cleanPrefix = prefix
+    .replace(/[^A-Z0-9]/gi, "")
+    .toUpperCase()
+    .slice(0, 3) // max 3 chars
+    .padEnd(3, "X"); // ensure 3 chars
+
+  const timestamp = Date.now().toString().slice(-9);
+
+  return `${cleanPrefix}${timestamp}${nanoid()}`;
 };
